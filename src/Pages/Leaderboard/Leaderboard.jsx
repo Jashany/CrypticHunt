0afce600632d409d4371bf272b14ff9259826d93
Jashany <jsingh12_be22@thapar.edu>
2024-11-24 17:58:36 +0530

    ans

--- src/Pages/Leaderboard/Leaderboard.jsx
+++ src/Pages/Leaderboard/Leaderboard.jsx
@@ -1,48 +1,49 @@
-import { useEffect, useState } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import styles from './Leaderboard.module.css';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./Leaderboard.module.css";
+import { useSelector } from "react-redux";
 const Leaderboard = () => {
   const { teamInfo } = useSelector((state) => state.team);
   const [leaderboardData, setLeaderboardData] = useState([]);
   useEffect(() => {
-    fetch('https://cryptic-api.acmtiet.com/leaderboard')
+    fetch("https://cryptic-api.acmtiet.com/leaderboard")
       .then((res) => res.json())
       .then((data) => {
-        setLeaderboardData(data.reverse());
-  })
+        setLeaderboardData(data);
+      })
       .catch((err) => console.log(err));
   }, []);
-   
-     
-      return (
-        <>
-          <Navbar />
-        <div className={styles.main}>
-          <div className={styles.heading}>
-          <h3 className={styles.teamscore}>Your Team_score: {teamInfo?.score}</h3>
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.main}>
+        <div className={styles.heading}>
+          <h3 className={styles.teamscore}>
+            Your Team_score: {teamInfo?.score}
+          </h3>
           <h2>Leaderboard</h2>
-          </div>
-        
-          <table>
-        <thead>
-          <tr>
-            <th>Team Name</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {leaderboardData?.slice(0,9).map((team, index) => (
-            <tr key={index}>
-          <td>{team?.team}</td>
-          <td>{Math.floor(team?.score)}</td>
-            </tr>
-          ))}
-        </tbody>
-          </table>
         </div>
-          </>
-      );
-    }
- 
+
+        <table>
+          <thead>
+            <tr>
+              <th>Team Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leaderboardData?.map((team, index) => (
+              <tr key={index}>
+                <td>{team?.team}</td>
+                <td>{Math.floor(team?.score)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
 export default Leaderboard;
