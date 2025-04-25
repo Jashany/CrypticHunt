49a080b8d81bf9200fe1fd0a70de05dfc008a379
Jashanpreet Singh <119065243+Jashany@users.noreply.github.com>
2025-04-25 21:09:15 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className={Style.main}>
             <ParticlesComponent />
             <Hero />
-            <Timer />
+{/*             <Timer /> */}
             {!isMobile && <Timeline />}
             {/* <Sponser /> */}
             {/* <Contact />      */}
