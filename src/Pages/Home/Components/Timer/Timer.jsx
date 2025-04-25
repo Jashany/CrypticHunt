4fded802d88d3f8c540299b2dd19a9f3eb143550
Jashanpreet Singh <119065243+Jashany@users.noreply.github.com>
2025-04-25 21:07:08 +0530

    Update Timer.jsx

--- src/Pages/Home/Components/Timer/Timer.jsx
+++ src/Pages/Home/Components/Timer/Timer.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import styles from "./Timer.module.css";
 
 const calculateTimeRemaining = () => {
-  const targetDate = new Date("April 25, 2025 22:00:00 GMT+00:00");
+  const targetDate = new Date("April 25, 2025 22:00:00 GMT+05:30");
   const now = new Date();
 
   const timeRemaining = targetDate - now;
